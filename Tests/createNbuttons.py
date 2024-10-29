90a42b6987eab1b1164673a8c37a231f72f50f07
rikkarlo <45422977+rikkarlo@users.noreply.github.com>
2024-10-29 15:23:16 +0100

    Add files via upload

--- Tests/createNbuttons.py
+++ Tests/createNbuttons.py
@@ -12,20 +12,17 @@ class UncertaintyPanel(bpy.types.Panel):
         scene = context.scene
 
         # Slider per selezionare il numero di bottoni
-        layout.prop(scene, "uncertainty_scale", text="Scala Incertezza", slider=True)
-
-        # Bottone per generare la scala
-        layout.operator("object.generate_uncertainty_scale", text="Genera scala incertezza")
+        layout.prop(scene, "uncertainty_levels", text="Uncertainty levels", slider=True)
 
         # Area per i bottoni generati
         box = layout.box()
-        for i in range(scene.uncertainty_scale):
+        for i in range(scene.uncertainty_levels):
             box.operator("object.uncertainty_button_{}".format(i + 1), text=str(i + 1))
 
 # Operatore per la generazione della scala
 class GenerateUncertaintyScaleOperator(bpy.types.Operator):
-    bl_idname = "object.generate_uncertainty_scale"
-    bl_label = "Genera Scala Incertezza"
+    bl_idname = "object.generate_uncertainty_levels"
+    bl_label = "Generate uncertainty scale"
 
     def execute(self, context):
         return {'FINISHED'}
@@ -37,35 +34,35 @@ def create_dynamic_button_class(index):
         bl_label = "Uncertainty Button {}".format(index)
 
         def execute(self, context):
-            self.report({'INFO'}, f"Bottone {index} cliccato!")
+            self.report({'INFO'}, f"Button {index} clicked!")
             return {'FINISHED'}
     return DynamicButton
 
 # Registrazione dinamica delle classi dei bottoni
 def register_buttons():
-    for i in range(1, 8):
+    for i in range(1, 11):
         bpy.utils.register_class(create_dynamic_button_class(i))
 
 def unregister_buttons():
-    for i in range(1, 8):
+    for i in range(1, 11):
         try:
             bpy.utils.unregister_class(create_dynamic_button_class(i))
         except RuntimeError:
             pass
 
 def register():
-    bpy.types.Scene.uncertainty_scale = bpy.props.IntProperty(
-        name="Scala Incertezza",
+    bpy.types.Scene.uncertainty_levels = bpy.props.IntProperty(
+        name="Uncertainty Scale",
         default=3,
-        min=3,
-        max=7
+        min=2,
+        max=10
     )
     bpy.utils.register_class(UncertaintyPanel)
     bpy.utils.register_class(GenerateUncertaintyScaleOperator)
     register_buttons()
 
 def unregister():
-    del bpy.types.Scene.uncertainty_scale
+    del bpy.types.Scene.uncertainty_levels
     bpy.utils.unregister_class(UncertaintyPanel)
     bpy.utils.unregister_class(GenerateUncertaintyScaleOperator)
     unregister_buttons()
